a819351e71d80fd969c2fa220432c7fb942f391b
MaksTkachenko <maksym_pro@ukr.net>
2023-08-09 22:36:58 +0300

    added task_5

---
+++ task_5.py
@@ -0,0 +1,31 @@
+# Завдання 5: Перевизначення методів
+
+class Shape:
+
+    def __init__(self, color):
+        self.color = color
+
+    def display_color(self):
+        print(f"Color: {self.color}")
+
+
+class Rectangle(Shape):
+    def __init__(self, color, width, height):
+        super().__init__(color)
+        self.width = width
+        self.height = height
+
+    def width(self):
+        return self.width
+
+    def height(self):
+        return self.height
+
+    def display_color(self):
+        super().display_color()
+        print(f"Color: {self.color}, Width: {self.width}, Height: {self.height}")
+
+
+rectangle = Rectangle("Blue", 10, 5)
+rectangle.display_color()  # Виведе "Color: Blue, Width: 10, Height: 5"
+
